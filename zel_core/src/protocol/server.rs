94709224a12c02e01d7dd52bcfa1798ef75a3220
hazel <hazelrtech@gmail.com>
2025-12-04 18:38:55 +0000

    feat: prep for publish

--- zel_core/src/protocol/server.rs
+++ zel_core/src/protocol/server.rs
@@ -3,6 +3,7 @@ use bytes::Bytes;
 use futures::{SinkExt, StreamExt};
 use iroh::{endpoint::Connection, protocol::ProtocolHandler};
 use log::{trace, warn};
+use std::future::Future;
 use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 
 use crate::protocol::{
