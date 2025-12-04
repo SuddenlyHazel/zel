b444e9ef01b7e8b4c41630c44d1deb3326a481d0
hazel <hazelrtech@gmail.com>
2025-12-04 18:47:40 +0000

    fix: export shutdown subscriber

--- zel_core/src/lib.rs
+++ zel_core/src/lib.rs
@@ -1,5 +1,7 @@
 mod iroh_helpers;
 
-pub use iroh_helpers::{Builder as BundleBuilder, IrohBundle};
+pub use iroh_helpers::{
+    Builder as BundleBuilder, BuilderError, IrohBundle, ShutdownListener, ShutdownReplier,
+};
 
 pub mod protocol;
