8dbaa2f94581bc93ee0da77f93086100880130fc
hazel <hazelrtech@gmail.com>
2025-12-05 13:23:45 +0000

    chore: fix doctest, inherant workspace specs

--- zel_macros/src/lib.rs
+++ zel_macros/src/lib.rs
@@ -10,12 +10,14 @@ mod service;
 ///
 /// # Example
 ///
-/// ```rust
+/// ```rust,ignore
+/// use zel_macros::zel_service;
+///
 /// #[zel_service(name = "math")]
 /// trait Math {
 ///     #[method(name = "add")]
 ///     async fn add(&self, a: i32, b: i32) -> Result<i32, String>;
-///     
+///
 ///     #[subscription(name = "counter")]
 ///     async fn counter(&self, interval_ms: u64) -> Result<(), String>;
 /// }
